a3e9ed90ff68b8218769dd5e6125c97091daa84c
ru0ya <viktaangi02@gmail.com>
2022-09-20 23:18:42 +0300

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def islower(c):
-    if(ord(c) == 99):
+def islowe(c):
+    if (ord(c) == 99):
         return True
-    elif(ord(c) != 99):
+    elif (ord(c) != 99):
         return False
