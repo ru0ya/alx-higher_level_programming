e18e58d8b2001a6df5e2d818287ca8a6606ca1ea
ru0ya <viktaangi02@gmail.com>
2022-09-27 21:19:19 +0300

    debugging

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main_":
     n = len(sys.argv)
 
     if n == 1:
-        print(f"{n} argument.", n- 1)
+        print(f"{} argument.", n- 1)
     elif n == 2:
-        print(f"{n} arguments:", n - 1)
+        print(f"{} arguments:", n - 1)
     else:
-        print(f"{n} arguments:", n - 1)
+        print(f"{} arguments:", n - 1)
 
     for i in range(1, n):
         print("{}:{}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "_main_":
     from variable_load_5 import a
-    print("{:d}".format(a))
+    print(f"{a:d}")
