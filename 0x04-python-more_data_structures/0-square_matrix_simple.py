3f4c4feac381486f1424c46d966413b999489e05
ru0ya <viktaangi02@gmail.com>
2022-09-29 16:08:39 +0300

    sorted dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
-    for i, square in enumerate(matrix)
+    for i, square in enumerate(matrix):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return(new_matrix)
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    result = 2 * len(a_dictionary)
+    return(result)
