0f33c39e23f2a6993abd5492cba6c59fe04d18ee
ru0ya <viktaangi02@gmail.com>
2022-10-11 21:20:51 +0300

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="\n")
             a += 1
-        except:
+        except ValueError:
             break
         print()
-    return(a)
+    return (a)
