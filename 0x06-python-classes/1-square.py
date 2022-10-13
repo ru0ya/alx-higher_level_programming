cc71d7efe54c79c0b54fcdc554297127444c533c
ru0ya <viktaangi02@gmail.com>
2022-10-13 22:23:30 +0300

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        """defines private instance attribute size"""
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """defines size instance"""
+        if not isinstance(size, int):
+            """checks for errors"""
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >=0")
+
+            self.__size = size
