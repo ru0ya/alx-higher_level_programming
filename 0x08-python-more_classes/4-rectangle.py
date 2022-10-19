d948fb3ce06802c6f542540bd1e73030ae12b79b
ru0ya <viktaangi02@gmail.com>
2022-10-19 15:48:07 +0300

    draw

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+class Rectangle:
+    """defines a class rectangle"""
+    def __init__(self, width=0, height=0):
+        """initializes class rectangle
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+            Raises:
+                TypeError: if width and height is not integer
+                ValueError: if width and height is not integer"""
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """retrieves  attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    @property
+
+    def width(self):
+        """retrieves attribute width"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """sets width attribute"""
+                                                                                                                                    1,18          Top
+
+ if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+    def area(self):
+        """returns area of rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns perimeter of rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return(0)
+        return ((self.__width + self.__height) *2)
+    def __str__(self) -> str:
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle += "#"
+                if i == 0 or j == 0:
+                    rectangle = ""
+        return(rectangle)
+
