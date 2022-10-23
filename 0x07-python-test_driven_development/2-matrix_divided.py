d7ef04ff5a34a6b965bba38518c244e7f1ac92a3
ru0ya <viktaangi02@gmail.com>
2022-10-23 22:45:44 +0300

    mattrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines elemsnts of a marix"""
+
+
+def matrix_divided(matrix, div):
+    if type(marix)is not list or len(matgrix):
+
+    for rows in matrix:
+        if type(rows) is not list:
+            raise TypeError(message)
