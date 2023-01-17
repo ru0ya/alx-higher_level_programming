81a2f7c7b8a3bd7d0b552c683dc842583118c8ce
ru0ya <viktaangi02@gmail.com>
2023-01-18 02:03:40 +0300

    error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
             print("[{}] {}".format(json.get("id"), json.get("name")))
         else:
             print("No result")
-    except ValueError:
+    except:
         print("Not a valid JSON")
