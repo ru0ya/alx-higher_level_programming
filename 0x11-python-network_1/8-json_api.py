9eca6536b51436f7b24130233f7f325c3bcf0698
ru0ya <viktaangi02@gmail.com>
2023-01-18 02:05:24 +0300

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """Python script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter."""
 
@@ -14,5 +14,5 @@ if __name__ == "__main__":
             print("[{}] {}".format(json.get("id"), json.get("name")))
         else:
             print("No result")
-    except:
+    except ValueError:
         print("Not a valid JSON")
