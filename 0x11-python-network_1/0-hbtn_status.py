3774ba80741d396fb69b9e4f69203a7dfa778215
ru0ya <viktaangi02@gmail.com>
2023-01-19 12:52:54 +0300

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-fetches a url"""
-
-
+# script that fetches https://alx-intranet.hbtn.io/status
+# You must use the package urllib
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
-    as reply:
-        data = reply.read()
-        print(" Body response:")
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data.decode("utf-8")))
+    #req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    #with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        #reading the content of the response as html var
+        html = response.read()
+        #printing html content
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
