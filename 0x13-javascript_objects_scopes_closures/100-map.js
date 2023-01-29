9879c3db21d9b3c0bb315dd9cc73e142cf2d4ea2
ru0ya <viktaangi02@gmail.com>
2023-01-29 22:18:49 +0300

    :runner:

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+// script that imports an array and computes a new array.
+
+const originalList = require('./100-data').list;
+console.log(originalList);
+const mappedList = originalList.map(function (e, index) {
+  return (e * index);
+});
+console.log(mappedList);
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-/* class Square which inherits from Rectangle class */
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c = 'X') {
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
